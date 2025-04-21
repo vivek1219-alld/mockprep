b4caa492edce92e6602fb6f609d341c4cd662ead
Vivek Singh <viveksingh1219@gmail.com>
2025-04-22 00:46:08 +0530

    fix: changed for root directory in vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,16 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  root: path.resolve(__dirname, "client"), // ✅ base folder
   base: './',
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    emptyOutDir: true
+    outDir: path.resolve(__dirname, "dist"), // ✅ this is what Vercel uses
+    emptyOutDir: true,
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "client", "src"),
     },
   }
 });
